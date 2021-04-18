a7321c17c1eb854c262b9f52a0a0a1cb7522c9b7
rebeccatricker <rebeccaatricker@gmail.com>
2021-04-18 10:00:57 -0500

    fixing files

---
+++ StarterCode/app.js
@@ -0,0 +1,10 @@
+// Create a function for Data plotting
+function metadata(id) {
+
+// Read in the json data
+    d3.json("../samples.json").then((sampleData) => {
+
+        console.log(sampleData);
+    }
+
+}
